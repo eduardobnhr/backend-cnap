44fa3f81b8620c36fcd5464e020696a5234601fd
eduardobnhr <eduardobnhr@gmail.com>
2024-01-27 09:32:46 -0400

    configuracao do processor

--- src/main/java/br/com/cnab/backend/Transacao.java
+++ src/main/java/br/com/cnab/backend/Transacao.java
@@ -2,6 +2,8 @@ package br.com.cnab.backend;
 
 import java.math.BigDecimal;
 import java.sql.Time;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public record Transacao(
@@ -14,5 +16,35 @@ public record Transacao(
     Time hora,
     String donoDaLoja,
     String nomeDaLoja) {
+
+        public Transacao withValor(BigDecimal valor){
+            return new Transacao(
+                this.id(), this.tipo(), this.data(),
+                valor,
+                this.cpf(), this.cartao(), this.hora(),
+                this.donoDaLoja(), this.nomeDaLoja()); //
+        }
+
+        public Transacao withData(String data) throws ParseException{
+            var dateFormat = new SimpleDateFormat("yyyyMMdd");
+            var date = dateFormat.parse(data);
+
+            return new Transacao(
+                this.id, this.tipo(), new Date(date.getTime()),
+                this.valor(), this.cpf(),
+                this.cartao(), this.hora(), this.donoDaLoja(),
+                this.nomeDaLoja());
+        }
+
+        public Transacao withHora(String hora) throws ParseException {
+            var dateFormat = new SimpleDateFormat("HHmmss");
+            var date =dateFormat.parse(hora);
+
+            return new Transacao(
+                this.id(), this.tipo(), this.data(),
+                this.valor(), this.cpf(), this.cartao(),
+                new Time(date.getTime()), this.donoDaLoja(), this.nomeDaLoja()
+                );
+        }
     
 }
